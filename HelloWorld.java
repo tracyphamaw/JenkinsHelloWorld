44b35de76f40d663e490f2dff06a6c3833254c37
tracyphamaw <96677490+tracyphamaw@users.noreply.github.com>
2022-01-29 09:52:52 +0800

    Update HelloWorld.java
    test webhook 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("-----***** Trying Webhook on localhost *****-----");
+		System.out.println("-----***** Trying Webhook on localhost 2 *****-----");
 	}
 }
